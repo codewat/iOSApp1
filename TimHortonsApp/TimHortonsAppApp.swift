7db383d3bc140d2885b849284cbbe3bc02a84b6c
codewat <cemar.watin@triosstudent.com>
2026-05-21 18:22:15 -0400

    Initial Commit

---
+++ TimHortonsApp/TimHortonsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimHortonsAppApp.swift
+//  TimHortonsApp
+//
+//  Created by Cemar on 2026-05-21.
+//
+
+import SwiftUI
+
+@main
+struct TimHortonsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
